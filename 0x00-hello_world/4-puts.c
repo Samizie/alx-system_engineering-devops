790c2e0af2990c6ed8db9b4f3960a1c6cc9ad172
Samizie <samilincoln@hotmail.com>
2022-09-09 14:00:28 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Entry point.
  *
- * Description: main - Prints out a string to stdout
+ * Description: main - Prints out a string to stdout.
  * Return : 0 if success.
  */
 int main(void)

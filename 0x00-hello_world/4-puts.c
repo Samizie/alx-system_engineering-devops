ef809b452ddddc9fbd7eccabf35ba807139dc6f8
Samizie <samilincoln@hotmail.com>
2022-09-09 13:57:28 -0700

    newly addded

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: main- Prints out a string to stdout
+ * Description: main - Prints out a string to stdout
  * Return : 0 if success.
  */
 int main(void)

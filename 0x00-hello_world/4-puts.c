3fdaddaebea2e97ba3c406686daf495d963f5bd1
Samizie <samilincoln@hotmail.com>
2022-09-09 14:16:29 -0700

    printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point.
- *
- * Description: main - Prints out a string to stdout.
+ * main - Prints out a string to stdout.
  * Return : 0 if success.
  */
 int main(void)

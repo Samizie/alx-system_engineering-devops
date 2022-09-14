b042284cefada16ec5b36a53e4a18985d5fd7ffd
Samizie <samilincoln@hotmail.com>
2022-09-14 06:09:45 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ *
+ * Return: 0
+ * /
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('P');
+	_putchar('U');
+	_putchar('T');
+	_putchar('C');
+	_putchar('H');
+	_putchar('A');
+	_putchar('R');
+
+	return (0);
+
+}
+

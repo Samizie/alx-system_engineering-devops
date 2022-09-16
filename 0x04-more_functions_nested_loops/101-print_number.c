75a917660662e59e00ab3f9e9d5374c8634dcdf1
Samizie <samilincoln@hotmail.com>
2022-09-16 01:36:51 -0700

    newly coded

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,44 @@
  */
 void print_number(int n)
 {
+	long m; /* power of 10 */
+	int c; /* boolean check */
+	long num; /* convert int to long */
+
+	num = n;
 	
-	return (0);
+	/* negatives */
+	if (num < 0)
+	{
+		num *= -1;
+		_putchar('-');
+
+	}
+
+	/* count up */
+	m = 1;
+	c = 1;
+	while (c)
+	{
+		if (num / (m * 10) > 0)
+			m *= 10;
+		else
+			c = 10;
+	}
+
+	/* count down */
+	while (num >= 0)
+	{
+		if (m == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / m % 10) + '0');
+			m /= 10;
+
+		}
+	}
 }

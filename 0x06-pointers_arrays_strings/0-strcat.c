7b64c98e1a2dd1ee97f7d628b0e37b6f0fddf25f
Samizie <samilincoln@hotmail.com>
2022-09-22 03:35:41 -0700

    anotheer amend

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,8 +6,7 @@
  * Return: destination string concatenated
  */
 char *_strcat(char *dest, char *src)
-{
-	int i = 0, j;
+{int i = 0, j;
 	while (dest[i] != '\0')
 		i++;
 	for (j = 0; src[j] != '\0'; j++, i++)

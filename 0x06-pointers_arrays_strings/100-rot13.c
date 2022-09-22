14b1e711d10ecc63783c593869d88a11241e4cc4
Samizie <samilincoln@hotmail.com>
2022-09-22 04:10:59 -0700

    rot 2

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,6 +8,7 @@ char *rot13(char *s)
 {int i, j;
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot_it[] =   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	
 	for (i = 0; s[i] != '\0'; i++)
 		for (j = 0; j < 52; j++)
 			if (s[i] == alphabet[j])
